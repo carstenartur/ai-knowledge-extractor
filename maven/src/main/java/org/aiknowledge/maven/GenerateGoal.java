841e86edd738bc6307cabd16e0beb1ed59f4a46b
Carsten Hammer <carsten.hammer@t-online.de>
2026-06-27 07:01:38 +0200

    refactor: move Maven generate goal to neutral package

---
+++ maven/src/main/java/org/aiknowledge/maven/GenerateGoal.java
@@ -0,0 +1,14 @@
+package org.aiknowledge.maven;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+public final class GenerateGoal extends AbstractAiKnowledgeMojo {
+    @Override
+    public void execute() throws MojoExecutionException {
+        try {
+            runner().generate(options());
+        } catch (Exception ex) {
+            throw new MojoExecutionException("Generation failed", ex);
+        }
+    }
+}

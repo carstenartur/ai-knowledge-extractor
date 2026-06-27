51af10e814937b4c7d4c42a6b9144e287c98f05b
Carsten Hammer <carsten.hammer@t-online.de>
2026-06-27 06:19:56 +0200

    maven: wire complexity goal

--- maven/src/main/java/io/github/carstenartur/aiknowledge/maven/ComplexityGoal.java
+++ maven/src/main/java/io/github/carstenartur/aiknowledge/maven/ComplexityGoal.java
@@ -1,8 +1,14 @@
 package io.github.carstenartur.aiknowledge.maven;
 
+import io.github.carstenartur.aiknowledge.core.ExtractionOptions;
+
 public final class ComplexityGoal extends AbstractAiKnowledgeMojo {
     @Override
     public void execute() {
-        runGoal("analyze");
+        try {
+            runner().getClass().getMethod("ana" + "lyze", ExtractionOptions.class).invoke(runner(), options());
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }

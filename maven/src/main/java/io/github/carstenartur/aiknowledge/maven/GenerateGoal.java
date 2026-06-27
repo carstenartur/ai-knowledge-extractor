5c438d60e9760d61f598ad1950bd207c37f5e5a4
Carsten Hammer <carsten.hammer@t-online.de>
2026-06-27 07:08:45 +0200

    cleanup: neutralize old Maven generate source

--- maven/src/main/java/io/github/carstenartur/aiknowledge/maven/GenerateGoal.java
+++ maven/src/main/java/io/github/carstenartur/aiknowledge/maven/GenerateGoal.java
@@ -1,14 +1,4 @@
-package io.github.carstenartur.aiknowledge.maven;
+package org.aiknowledge.legacy.maven;
 
-import io.github.carstenartur.aiknowledge.core.AiKnowledgeRunner;
-
-public final class GenerateGoal extends AbstractAiKnowledgeMojo {
-    @Override
-    public void execute() throws org.apache.maven.plugin.MojoExecutionException {
-        try {
-            new AiKnowledgeRunner().generate(options());
-        } catch (Exception ex) {
-            throw new org.apache.maven.plugin.MojoExecutionException("Generation failed", ex);
-        }
-    }
+final class RemovedGenerateGoal {
 }

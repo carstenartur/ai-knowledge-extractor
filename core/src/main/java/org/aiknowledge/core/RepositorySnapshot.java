14c2b82a77fca8e326373356e17ee5d7738123c1
Carsten Hammer <carsten.hammer@t-online.de>
2026-06-27 06:58:47 +0200

    refactor: move snapshot to neutral package

---
+++ core/src/main/java/org/aiknowledge/core/RepositorySnapshot.java
@@ -0,0 +1,16 @@
+package org.aiknowledge.core;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public final class RepositorySnapshot {
+    public final Map index = new LinkedHashMap();
+    public final ArrayList modules = new ArrayList();
+    public final ArrayList classes = new ArrayList();
+    public final ArrayList tests = new ArrayList();
+    public final ArrayList docs = new ArrayList();
+    public final ArrayList dependencies = new ArrayList();
+    public final ArrayList capabilities = new ArrayList();
+    public final ArrayList claims = new ArrayList();
+}

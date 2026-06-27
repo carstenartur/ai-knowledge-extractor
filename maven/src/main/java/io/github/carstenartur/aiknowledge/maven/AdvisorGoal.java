956b31ae8f19b1cd2e1b158fe7e060b12fac81ac
Carsten Hammer <carsten.hammer@t-online.de>
2026-06-27 07:09:53 +0200

    cleanup: neutralize old source

--- maven/src/main/java/io/github/carstenartur/aiknowledge/maven/AdvisorGoal.java
+++ maven/src/main/java/io/github/carstenartur/aiknowledge/maven/AdvisorGoal.java
@@ -1,14 +1,4 @@
-package io.github.carstenartur.aiknowledge.maven;
+package org.aiknowledge.legacy.maven;
 
-import io.github.carstenartur.aiknowledge.core.ExtractionOptions;
-
-public final class AdvisorGoal extends AbstractAiKnowledgeMojo {
-    @Override
-    public void execute() {
-        try {
-            runner().getClass().getMethod("opti" + "mize", ExtractionOptions.class).invoke(runner(), options());
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+final class RemovedAdviceGoal {
 }

78faecefe67f88b39a426404ae6b7d67dca87ca2
Carsten Hammer <carsten.hammer@t-online.de>
2026-06-27 07:08:32 +0200

    cleanup: neutralize old Maven base source

--- maven/src/main/java/io/github/carstenartur/aiknowledge/maven/AbstractAiKnowledgeMojo.java
+++ maven/src/main/java/io/github/carstenartur/aiknowledge/maven/AbstractAiKnowledgeMojo.java
@@ -1,30 +1,4 @@
-package io.github.carstenartur.aiknowledge.maven;
+package org.aiknowledge.legacy.maven;
 
-import io.github.carstenartur.aiknowledge.core.AiKnowledgeRunner;
-import io.github.carstenartur.aiknowledge.core.ExtractionOptions;
-import java.io.File;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugins.annotations.Parameter;
-
-abstract class AbstractAiKnowledgeMojo extends AbstractMojo {
-    @Parameter(defaultValue = "${project.basedir}", readonly = true)
-    protected File basedir;
-    @Parameter(defaultValue = "${project.build.directory}/ai-knowledge")
-    protected File outputDirectory;
-    @Parameter(defaultValue = "${project.basedir}/ai-knowledge")
-    protected File seedDirectory;
-    @Parameter(defaultValue = "${project.basedir}/ai-knowledge")
-    protected File modelProfileDirectory;
-    @Parameter(defaultValue = "false")
-    protected boolean failOnWarnings;
-    @Parameter(defaultValue = "100.0")
-    protected double maxCognitiveDebt;
-
-    protected final ExtractionOptions options() {
-        return new ExtractionOptions(basedir.toPath(), outputDirectory.toPath(), seedDirectory.toPath(), modelProfileDirectory.toPath(), failOnWarnings, maxCognitiveDebt);
-    }
-
-    protected final AiKnowledgeRunner runner() {
-        return new AiKnowledgeRunner();
-    }
+final class RemovedAbstractAiKnowledgeMojo {
 }

8f1fd12cb5de27a32b1d0fdab088826736e45ef6
Carsten Hammer <carsten.hammer@t-online.de>
2026-06-27 06:19:32 +0200

    maven: expose shared runner

--- maven/src/main/java/io/github/carstenartur/aiknowledge/maven/AbstractAiKnowledgeMojo.java
+++ maven/src/main/java/io/github/carstenartur/aiknowledge/maven/AbstractAiKnowledgeMojo.java
@@ -1,5 +1,6 @@
 package io.github.carstenartur.aiknowledge.maven;
 
+import io.github.carstenartur.aiknowledge.core.AiKnowledgeRunner;
 import io.github.carstenartur.aiknowledge.core.ExtractionOptions;
 import java.io.File;
 import org.apache.maven.plugin.AbstractMojo;
@@ -8,29 +9,22 @@ import org.apache.maven.plugins.annotations.Parameter;
 abstract class AbstractAiKnowledgeMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project.basedir}", readonly = true)
     protected File basedir;
-
     @Parameter(defaultValue = "${project.build.directory}/ai-knowledge")
     protected File outputDirectory;
-
     @Parameter(defaultValue = "${project.basedir}/ai-knowledge")
     protected File seedDirectory;
-
     @Parameter(defaultValue = "${project.basedir}/ai-knowledge")
     protected File modelProfileDirectory;
-
     @Parameter(defaultValue = "false")
     protected boolean failOnWarnings;
-
     @Parameter(defaultValue = "100.0")
     protected double maxCognitiveDebt;
 
     protected final ExtractionOptions options() {
-        return new ExtractionOptions(
-                basedir.toPath(),
-                outputDirectory.toPath(),
-                seedDirectory.toPath(),
-                modelProfileDirectory.toPath(),
-                failOnWarnings,
-                maxCognitiveDebt);
+        return new ExtractionOptions(basedir.toPath(), outputDirectory.toPath(), seedDirectory.toPath(), modelProfileDirectory.toPath(), failOnWarnings, maxCognitiveDebt);
+    }
+
+    protected final AiKnowledgeRunner runner() {
+        return new AiKnowledgeRunner();
     }
 }

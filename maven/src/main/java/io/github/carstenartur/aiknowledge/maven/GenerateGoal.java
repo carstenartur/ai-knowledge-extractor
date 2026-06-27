c73a3d80431bec8b8bf5b09e618be0091e23da0b
Carsten Hammer <carsten.hammer@t-online.de>
2026-06-27 06:18:17 +0200

    maven: implement generate goal

--- maven/src/main/java/io/github/carstenartur/aiknowledge/maven/GenerateGoal.java
+++ maven/src/main/java/io/github/carstenartur/aiknowledge/maven/GenerateGoal.java
@@ -1,4 +1,14 @@
 package io.github.carstenartur.aiknowledge.maven;
 
-public final class GenerateGoal {
+import io.github.carstenartur.aiknowledge.core.AiKnowledgeRunner;
+
+public final class GenerateGoal extends AbstractAiKnowledgeMojo {
+    @Override
+    public void execute() throws org.apache.maven.plugin.MojoExecutionException {
+        try {
+            new AiKnowledgeRunner().generate(options());
+        } catch (Exception ex) {
+            throw new org.apache.maven.plugin.MojoExecutionException("Generation failed", ex);
+        }
+    }
 }

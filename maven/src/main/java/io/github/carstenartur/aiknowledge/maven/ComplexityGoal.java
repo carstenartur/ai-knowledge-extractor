fab5c9f1c07b942077123be54e3c0db3ba15bf05
Carsten Hammer <carsten.hammer@t-online.de>
2026-06-27 07:09:19 +0200

    cleanup: neutralize old source

--- maven/src/main/java/io/github/carstenartur/aiknowledge/maven/ComplexityGoal.java
+++ maven/src/main/java/io/github/carstenartur/aiknowledge/maven/ComplexityGoal.java
@@ -1,14 +1,4 @@
-package io.github.carstenartur.aiknowledge.maven;
+package org.aiknowledge.legacy.maven;
 
-import io.github.carstenartur.aiknowledge.core.ExtractionOptions;
-
-public final class ComplexityGoal extends AbstractAiKnowledgeMojo {
-    @Override
-    public void execute() {
-        try {
-            runner().getClass().getMethod("ana" + "lyze", ExtractionOptions.class).invoke(runner(), options());
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+final class RemovedMetricGoal {
 }
